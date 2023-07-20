70671d8cfc412dbcda2caf5f6160cb465e60c078
82109 <emma030126@gmaul.com>
2023-07-20 16:21:45 +0900

    영화정보2

---
+++ app/src/main/java/com/example/contentrecommendation/Result_romance.kt
@@ -0,0 +1,11 @@
+package com.example.contentrecommendation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Result_romance : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_romance)
+    }
+}
