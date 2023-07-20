6ae209ab85408c2ffb195185688784e410821b7b
yebin <didbdb32@naver.com>
2023-07-20 16:58:39 +0900

    범죄물

---
+++ app/src/main/java/com/example/contentrecommendation/Result_criminal.kt
@@ -0,0 +1,11 @@
+package com.example.contentrecommendation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Result_criminal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_ciminal)
+    }
+}
