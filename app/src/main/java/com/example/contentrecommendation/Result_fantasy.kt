505ab92c3141a2e287f665cc385e07650bad6361
yebin <didbdb32@naver.com>
2023-07-23 00:31:01 +0900

    네개완료

---
+++ app/src/main/java/com/example/contentrecommendation/Result_fantasy.kt
@@ -0,0 +1,11 @@
+package com.example.contentrecommendation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Result_fantasy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_fantasy)
+    }
+}
