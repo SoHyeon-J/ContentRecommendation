6bafe6fa871b6a2bcc9d8c9b84cc3ca4580c3f23
sunkk <so1io@naver.com>
2023-07-24 09:59:49 +0900

    메인 시작 버튼 + 질문 1 이동

--- app/src/main/java/com/example/contentrecommendation/MainActivity.kt
+++ app/src/main/java/com/example/contentrecommendation/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.contentrecommendation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainStartBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainStartBtn = findViewById(R.id.mainStartBtn)
+
+        mainStartBtn.setOnClickListener {
+            var intent = Intent(this, test1::class.java )
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/contentrecommendation/test1.kt
+++ app/src/main/java/com/example/contentrecommendation/test1.kt
@@ -1,11 +1,28 @@
 package com.example.contentrecommendation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class test1 : AppCompatActivity() {
+
+    lateinit var upbutton1: Button
+    lateinit var downbutton1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test1)
+        upbutton1 = findViewById(R.id.upbutton1)
+        downbutton1 = findViewById(R.id.downbutton1)
+
+        upbutton1.setOnClickListener {
+            val intent = Intent(this, test2::class.java)
+            startActivity(intent)
+        }
+
+        downbutton1.setOnClickListener {
+            val intent = Intent(this, test2_2::class.java)
+            startActivity(intent)
+        }
     }
 }
