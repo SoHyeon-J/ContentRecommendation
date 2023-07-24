1b414563414f11401858222e092fed01a0e1d9ac
전소현 <shswu03@gmail.com>
2023-07-24 10:18:11 +0900

    전소현3

--- app/src/main/java/com/example/contentrecommendation/test3.kt
+++ app/src/main/java/com/example/contentrecommendation/test3.kt
@@ -1,11 +1,28 @@
 package com.example.contentrecommendation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class test3 : AppCompatActivity() {
+    lateinit var upbutton4: Button
+    lateinit var downbutton4: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test3)
+
+        upbutton4 = findViewById(R.id.upbutton4)
+        downbutton4 = findViewById(R.id.upbutton4)
+
+        upbutton4.setOnClickListener {
+            var intent = Intent(this, Result_horror::class.java)
+            startActivity(intent)
+        }
+
+        downbutton4.setOnClickListener {
+            var intent = Intent(this, Result_criminal::class.java)
+            startActivity(intent)
+        }
     }
 }
